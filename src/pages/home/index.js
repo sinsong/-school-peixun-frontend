bb512e3377fe3cbd8aaaf8fd45ec7112bfb2ec52
CSH <2389579869@qq.com>
2021-09-14 09:48:37 +0800

    请假功能新加字段

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,6 +5,7 @@ import "./index.less"
 import { createRef, useState } from "react"
 import { useHttp } from "../../utils/http"
 import { useAsync } from "../../utils/use-async"
+import * as auth from "../../auth-provider"
 
 
 const {Header, Content, Footer} = Layout
@@ -18,16 +19,7 @@ const Home = () => {
     const postAttendance = useHttp()
 
     const handleCalendarSelect = (date) => {
-        const currentDate = new Date();
-        const dateRange = (+date) - (+currentDate) // 和空字符串拼接，隐式转换成时间戳
-        if (dateRange >= 0)
-        {
-            setVisible(true)
-        }
-        else
-        {
-            message.info("无法为过去的时间请假")
-        }
+        setVisible(true)
     }
     const handleAttendaceInfo = () => {
         const value = form.current.getFieldValue()
@@ -52,6 +44,9 @@ const Home = () => {
                 break
             }
         }
+        value.userID = auth.getCurrentUserID() // 用于关联用户 id
+        // 状态标志 processing, success, reject
+        value.status = 'processing'
         run(postAttendance("660/attendance", {method: "POST", data: value})).then(()=>{
             message.success("已提交申请")
             setVisible(false)
@@ -88,7 +83,10 @@ const Home = () => {
             </Breadcrumb>
 
             <div className="site-layout-background" style={{padding: 24, minHeight: 380}}>
-                <Calendar onSelect={handleCalendarSelect} />
+                <div>
+                    <Button type="primary" onClick={handleCalendarSelect}>请假</Button>
+                </div>
+                <Calendar />
                 <Modal title="填写请假信息" width={800} visible={visible} footer={[
                     <Button type="primary" htmlType="submit" onClick={handleAttendaceInfo}>确定</Button>,
                     <Button onClick={()=>{setVisible(false); form.current.setFieldsValue({
