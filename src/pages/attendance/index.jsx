9853ec63affddad7061f198da3d82b06e6753936
CSH <2389579869@qq.com>
2021-09-14 17:10:41 +0800

    重构 Home，加入 react-router

---
+++ src/pages/attendance/index.jsx
@@ -0,0 +1,137 @@
+import { createRef, useEffect, useState } from "react"
+import { DatePicker, message, Badge, Button, Calendar, Modal, Form, Radio, Input, Breadcrumb } from "antd"
+import { useHttp } from "../../utils/http"
+import { useAsync } from "../../utils/use-async"
+import * as auth from "../../auth-provider"
+
+const { RangePicker } = DatePicker
+
+const Attendance = () => {
+  const [visible, setVisible] = useState(false) // 模态框的显示
+  const [attendanceList, setAttendanceList] = useState()
+  const form = createRef()
+
+  const { run, isLoading } = useAsync(undefined, { throwOnError: true })
+  const postAttendance = useHttp()
+  const getAttendanceList = useHttp()
+
+  useEffect(() => {
+    run(getAttendanceList("660/attendance", { data: { userID: auth.getCurrentUserID() } })).then(result => {
+      const list = new Map()
+      const currentMonth = (new Date()).getMonth()
+      result.forEach(item => {
+        const start = item.range.split("~")[0]
+        const startDate = new Date(start)
+        const month = startDate.getMonth()
+        const day = startDate.getDate()
+        if (month === currentMonth) {
+          list.has(day) ? list.get(day).push(item) : list.set(day, [item])
+        }
+
+      })
+      setAttendanceList(list)
+    })
+    setAttendanceList(new Map())
+  }, [])
+
+  const handleCalendarSelect = (date) => {
+    setVisible(true)
+  }
+
+  const handleAttendanceInfo = () => {
+    const value = form.current.getFieldValue()
+    const date = value.range[0].date()
+    if (value.range) {
+      const start = value.range[0].format("YYYY-MM-DD HH:mm")
+      const end = value.range[1].format("YYYY-MM-DD HH:mm")
+      value.range = `${start}~${end}`
+    }
+    if (value.type) {
+      switch (value.type) {
+        case 1:
+          value.type = "事假"
+          break
+        case 2:
+          value.type = "病假"
+          break
+        default:
+          value.type = "其他"
+          break
+      }
+    }
+    value.userID = auth.getCurrentUserID() // 用于关联用户 id
+    // 状态标志 processing, success, reject
+    value.status = 'processing'
+    run(postAttendance("660/attendance", { method: "POST", data: value })).then(() => {
+      message.success("已提交申请")
+      setVisible(false)
+      attendanceList.has(date) ? attendanceList.get(date).push(value) : attendanceList.set(date, [value])
+      const list = new Map(attendanceList)
+      setAttendanceList(list)
+    }).catch((error) => {
+      message.error(error.message)
+    })
+  }
+
+  const dateCellRender = (value) => {
+    if (!attendanceList)
+      return
+    const date = value.date()
+    if (attendanceList.has(date)) {
+      const list = attendanceList.get(date)
+      return <ul style={{ listStyle: "none" }}>
+        {
+          list.map((item, key) => {
+            return <li key={key}>
+              <Badge status={item.status} text={item.reason} />
+            </li>
+          })
+        }
+      </ul>
+    }
+  }
+  return (
+    <>
+      <Breadcrumb style={{ margin: "16px 0" }}>
+        <Breadcrumb.Item>主页</Breadcrumb.Item>
+        <Breadcrumb.Item >请假申请</Breadcrumb.Item>
+
+      </Breadcrumb>
+      <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
+        <div>
+          <Button type="primary" onClick={handleCalendarSelect}>请假</Button>
+        </div>
+        <Calendar dateCellRender={dateCellRender} />
+        <Modal title="填写请假信息" width={800} visible={visible} footer={[
+          <Button type="primary" htmlType="submit" onClick={handleAttendanceInfo}>确定</Button>,
+          <Button onClick={() => {
+            setVisible(false); form.current.setFieldsValue({
+              name: "", range: "", type: -1, reason: ""
+            })
+          }}>取消</Button>
+        ]}>
+          <Form onFinish={handleAttendanceInfo} ref={form}>
+            <Form.Item label="姓名" name="name">
+              <Input />
+            </Form.Item>
+            <Form.Item label="请假类型" name="type">
+              <Radio.Group>
+                <Radio value={1}>事假</Radio>
+                <Radio value={2}>病假</Radio>
+                <Radio value={3}>其他</Radio>
+              </Radio.Group>
+            </Form.Item>
+            <Form.Item label="请假时间" name="range">
+              <RangePicker showTime={{ format: "HH:mm" }} format="YYYY-MM-DD HH:mm" />
+            </Form.Item>
+            <Form.Item label="请假事由" name="reason">
+              <Input.TextArea rows={8} />
+            </Form.Item>
+          </Form>
+        </Modal>
+      </div>
+    </>
+  )
+}
+
+export default Attendance
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,59 +1,15 @@
 import { useAuth } from "../../context/auth-context"
-import { Breadcrumb, Button, Dropdown, Menu, Layout, Calendar, Modal, message, Form, Input, Radio, DatePicker } from "antd"
+import { Button, Dropdown, Menu, Layout } from "antd"
 import { DownOutlined, LogoutOutlined } from "@ant-design/icons"
 import "./index.less"
-import { createRef, useState } from "react"
-import { useHttp } from "../../utils/http"
-import { useAsync } from "../../utils/use-async"
-import * as auth from "../../auth-provider"
+import Attendance from "../attendance"
+import { Switch, Route, Link, BrowserRouter as Router, Redirect } from "react-router-dom"
 
-
-const {Header, Content, Footer} = Layout
-const { RangePicker } = DatePicker
+const { Header, Content, Footer } = Layout
 
 const Home = () => {
-    const [ visible, setVisible ] = useState(false) // 模态框的显示
     const { logout } = useAuth();
-    const form = createRef()
-    const { run, isLoading } = useAsync(undefined, { throwOnError: true })
-    const postAttendance = useHttp()
 
-    const handleCalendarSelect = (date) => {
-        setVisible(true)
-    }
-    const handleAttendaceInfo = () => {
-        const value = form.current.getFieldValue()
-        if (value.range)
-        {
-            const start = value.range[0].format("YYYY-MM-DD HH:mm")
-            const end = value.range[1].format("YYYY-MM-DD HH:mm")
-            value.range = `${start}~${end}`
-        }
-        if (value.type)
-        {
-            switch(value.type)
-            {
-            case 1:
-                value.type = "事假"
-                break
-            case 2:
-                value.type = "病假"
-                break
-            default:
-                value.type = "其他"
-                break
-            }
-        }
-        value.userID = auth.getCurrentUserID() // 用于关联用户 id
-        // 状态标志 processing, success, reject
-        value.status = 'processing'
-        run(postAttendance("660/attendance", {method: "POST", data: value})).then(()=>{
-            message.success("已提交申请")
-            setVisible(false)
-        }).catch((error)=>{
-            message.error(error.message)
-        })
-    }
     const menu = (
         <Menu>
             <Menu.Item>
@@ -61,61 +17,41 @@ const Home = () => {
             </Menu.Item>
         </Menu>
     )
-    return <Layout id="components-layout-demo-fixed">
-        <Header style={{position: 'fixed', zIndex: 1, width: "100%"}}>
-            <div className="logo">考勤管理系统</div>
-            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-                <Menu.Item key="1">nav 1</Menu.Item>
-                <Menu.Item key="2">nav 2</Menu.Item>
-                <Menu.Item key="3">nav 3</Menu.Item>
-            </Menu>
-            <div style={{position: "absolute", top: "0", right: "0"}}>
-                <Dropdown overlay={menu} trigger={["click"]}>
-                    <Button type="link" style={{paddingRight: 0}}>个人<DownOutlined /></Button>
-                </Dropdown>
-            </div>
-        </Header>
-        <Content className="site-layout" style={{padding: "0 50px", marginTop: 64}}>
-            <Breadcrumb style={{margin: "16px 0"}}>
-                <Breadcrumb.Item>Home</Breadcrumb.Item>
-                <Breadcrumb.Item >List</Breadcrumb.Item>
-                <Breadcrumb.Item>App</Breadcrumb.Item>
-            </Breadcrumb>
 
-            <div className="site-layout-background" style={{padding: 24, minHeight: 380}}>
-                <div>
-                    <Button type="primary" onClick={handleCalendarSelect}>请假</Button>
-                </div>
-                <Calendar />
-                <Modal title="填写请假信息" width={800} visible={visible} footer={[
-                    <Button type="primary" htmlType="submit" onClick={handleAttendaceInfo}>确定</Button>,
-                    <Button onClick={()=>{setVisible(false); form.current.setFieldsValue({
-                        name: "", range: "", type: -1, reason: ""
-                    })}}>取消</Button>
-                ]}>
-                    <Form onFinish={handleAttendaceInfo} ref={form}>
-                        <Form.Item label="姓名" name="name">
-                            <Input />
-                        </Form.Item>
-                        <Form.Item label="请假类型" name="type">
-                            <Radio.Group>
-                                <Radio value={1}>事假</Radio>
-                                <Radio value={2}>病假</Radio>
-                                <Radio value={3}>其他</Radio>
-                            </Radio.Group>
-                        </Form.Item>
-                        <Form.Item label="请假时间" name="range">
-                            <RangePicker showTime={{format: "HH:mm"}} format="YYYY-MM-DD HH:mm" />
-                        </Form.Item>
-                        <Form.Item label="请假事由" name="reason">
-                            <Input.TextArea rows={8}  />
-                        </Form.Item>
-                    </Form>
-                </Modal>
-            </div>
-        </Content>
-        <Footer style={{textAlign: "center"}}>考勤管理系统 CopyRight ©2021 All right reserved. </Footer>
-    </Layout>
+    return (
+        <Router>
+            <Layout id="components-layout-demo-fixed">
+                <Header style={{ position: 'fixed', zIndex: 1, width: "100%" }}>
+                    <div className="logo">考勤管理系统</div>
+                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+                        <Menu.Item key="1">
+                            <Link to="apply">请假申请</Link>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <Link to="manage">考勤管理</Link>
+                        </Menu.Item>
+                    </Menu>
+                    <div style={{ position: "absolute", top: "0", right: "0" }}>
+                        <Dropdown overlay={menu} trigger={["click"]}>
+                            <Button type="link" style={{ paddingRight: 0 }}>个人<DownOutlined /></Button>
+                        </Dropdown>
+                    </div>
+                </Header>
+                <Content className="site-layout" style={{ padding: "0 50px", marginTop: 64 }}>
+                    <Switch>
+                        <Route path="/apply">
+                            <Attendance />
+                        </Route>
+                        <Route path="/manage">
+                            {/* <AttendanceList /> */}
+                        </Route>
+                        <Redirect from="/" to="/apply"></Redirect>
+                    </Switch>
+                </Content>
+                <Footer style={{ textAlign: "center" }}>考勤管理系统 CopyRight ©2021 All right reserved. </Footer>
+            </Layout>
+        </Router>
+    )
 }
 
 export default Home
